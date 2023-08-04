703ca9d019b946ff1baf0ff4eff97e298d4ea2c0
Pramada Shrestha <pramadashrestha17@gmail.com>
2023-08-04 15:56:15 +0545

    jewellery

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,27 @@
 import Rating from "./Rating";
+import { Link } from "react-router-dom";
 
 const Card = ({ product }) => {
   return (
-    <div className=" bg-gray-50 hover:drop-shadow-lg hover:scale-105 p-4">
-      <img
-        className="aspect-[4/3] w-full rounded object-contain my-4"
-        src={product.image}
-        alt="img"
-      />
+    <Link to={`/products/${product.id}`}>
+      <div className="  hover:drop-shadow-lg hover:scale-105 p-4">
+        <img
+          className="aspect-[4/3] w-full rounded object-contain my-4"
+          src={product.image}
+          alt="img"
+        />
 
-      <div className="text-left text-xl ">{product.title}</div>
+        <div className="text-left text-xl ">{product.title}</div>
 
-      <div className="text-left text-2xl font-medium my-2">
-        $ {product.price}
+        <div className="text-left text-2xl font-medium my-2">
+          $ {product.price}
+        </div>
+
+        <div className="text-left ">
+          <Rating rate={product.rating.rate} />
+        </div>
       </div>
-
-      <div className="text-left text-base">
-        <Rating rate={product.rating.rate} />
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/Jewellery.js
+++ src/components/Jewellery.js
@@ -2,25 +2,22 @@ import { useEffect, useState } from "react";
 import { get } from "../Api";
 import Card from "./Card";
 
-const Hotpicks = () => {
+const Jewellery = () => {
   const [products, setProducts] = useState([]);
+  console.log("🚀 ~ file: Jewellery.js:7 ~ Jewellery ~ products:", products);
 
   const getProducts = async () => {
-    const res = await get("/products");
-    setProducts(res?.data.slice(0, 6));
+    const res = await get("/products/category/jewelery");
+    setProducts(res?.data);
   };
 
   useEffect(() => {
-    getProducts();
-  }, []);
+    getProducts(products);
+  });
 
   return (
-    <div>
-      <div className="text-4xl text-center  mb-4 text-[#001427]">
-        {" "}
-        Hot Picks{" "}
-      </div>
-
+    <div className="px-16 py-8">
+      <div className="text-4xl pb-8">All Jewelleries</div>
       <div className="grid grid-cols-1  md:grid-cols-4 lg:grid-cols-6 gap-4 ">
         {products.length > 0 &&
           products.map((product) => <Card product={product} />)}
@@ -29,4 +26,4 @@ const Hotpicks = () => {
   );
 };
 
-export default Hotpicks;
+export default Jewellery;
